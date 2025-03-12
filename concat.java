81f7b4ce9fb04fd6453984f6af6135b2f36dac9f
nishmarnishma <nishmar61@gmail.com>
2025-03-12 15:13:27 +0530

    concat

---
+++ concat.java
@@ -0,0 +1,8 @@
+public class concat {
+    public static void main(String[] args) {
+        String name1 = "Nishma";
+        String name2 = "Nishu";
+        System.out.println(name1.concat(name2));
+    }
+
+}
