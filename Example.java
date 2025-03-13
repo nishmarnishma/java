fd4b50a90330b9c340af7e213fc0d71b92148622
nishmarnishma <nishmar61@gmail.com>
2025-03-13 14:25:48 +0530

    Backslash escape character

---
+++ Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main(String[] args) {
+        String str = "There is \"an\" apple";
+        String str1 = "The \"butterfly\" was beautiful";
+        System.out.println(str);
+        System.out.println(str1);
+        // \backslash is used to write string inside the string
+    }
+}
