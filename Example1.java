9cd4cc5865840d5b60194acbc4c939a12dd38366
nishmarnishma <nishmar61@gmail.com>
2025-03-13 14:43:47 +0530

    Age

---
+++ Example1.java
@@ -0,0 +1,13 @@
+public class Example1 {
+    public static void main(String[] args) {
+        int age = 17;
+        
+        if (age >= 19)
+
+            System.out.println("He is Major");
+        else
+            System.out.println("He is minor");
+
+    }
+
+}
