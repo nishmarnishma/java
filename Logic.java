1ed48c64913b1aba6004a129fad53b0422e80cfa
nishmarnishma <nishmar61@gmail.com>
2025-03-13 12:01:31 +0530

    Logical Operator

---
+++ Logic.java
@@ -0,0 +1,9 @@
+public class Logic {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        System.out.println(a < b && a == b);
+        System.out.println(a < b || a == b);
+    }
+
+}
