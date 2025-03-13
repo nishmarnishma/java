b3cf4393f0c177be9d4caba61cc11d1879183e26
nishmarnishma <nishmar61@gmail.com>
2025-03-13 10:04:54 +0530

    Student mark

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static void main(String[] args) {
+        String name = "Nishma";
+        int mark = 90;
+        char grade = 'A';
+        System.out.println("Enter the student name" + name);
+        System.out.println("Enter the mark of the student" + mark);
+        System.out.println("Enter the grade of the student" + grade);
+    }
+}
