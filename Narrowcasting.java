3be344a350fc573eaa808d8d39ddd68741fa9803
nishmarnishma <nishmar61@gmail.com>
2025-03-13 10:41:21 +0530

    Narrowing Casting

---
+++ Narrowcasting.java
@@ -0,0 +1,8 @@
+public class Narrowcasting {
+    public static void main(String[] args) {
+        double mydouble = 5.34;
+        int myint = (int) mydouble;
+        System.out.println(myint);
+
+    }
+}
