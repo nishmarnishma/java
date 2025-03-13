71d53a5253a58f4e7186efe47ce632365b1a1acc
nishmarnishma <nishmar61@gmail.com>
2025-03-13 11:11:05 +0530

    Arithmatic Operation

---
+++ Arithmatic.java
@@ -0,0 +1,10 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 50;
+        int Addition = num1 + num2;
+        int Multiplication = num1 * num2;
+        System.out.println(Addition);
+        System.out.println(Multiplication);
+    }
+}
