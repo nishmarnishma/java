b4baaa0c5d6b7da03f0434ee3246cf3f5d1f1bb7
nishmarnishma <nishmar61@gmail.com>
2025-03-14 11:06:42 +0530

    Printing

---
+++ Lexample4.java
@@ -0,0 +1,10 @@
+public class Lexample4 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Apple");
+            i++;
+        } while (i <= 5);
+    }
+
+}
