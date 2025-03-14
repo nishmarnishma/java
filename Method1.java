ddd64e0acedeb5f970f691f5e4083aebde431c19
nishmarnishma <nishmar61@gmail.com>
2025-03-14 13:12:16 +0530

    Multiplication using Method

---
+++ Method1.java
@@ -0,0 +1,12 @@
+public class Method1 {
+    static void mymethod() {
+        int x = 25;
+        int y = 5;
+        int z = x * y;
+        System.out.println("The product of x and y is " + z);
+    }
+
+    public static void main(String[] args) {
+        mymethod();
+    }
+}
