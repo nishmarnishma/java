eb155b13bee493d61f7f5bc3389f09123e327451
nishmarnishma <nishmar61@gmail.com>
2025-03-14 10:34:58 +0530

    Printing 10 five times

---
+++ Lexample1.java
@@ -0,0 +1,9 @@
+public class Lexample1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(10);
+            i++;
+        }
+    }
+}
