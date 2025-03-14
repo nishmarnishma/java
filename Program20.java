10834f89f1e285cd0ce1b0cb1464e5bd4e57718e
nishmarnishma <nishmar61@gmail.com>
2025-03-14 14:48:34 +0530

    printing fruits using For loop

---
+++ Program20.java
@@ -0,0 +1,8 @@
+public class Program20 {
+    public static void main(String[] args) {
+        String[] fruits = { "Apple", "Orange", "Mango", "Jack fruit", "Pine Apple" };
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(fruits[i]);
+        }
+    }
+}
