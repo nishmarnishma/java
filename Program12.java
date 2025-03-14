c0855f1da45cd22cbe249daf633df2029d13e3ea
nishmarnishma <nishmar61@gmail.com>
2025-03-14 13:58:42 +0530

    Finding length using arrays

---
+++ Program12.java
@@ -0,0 +1,6 @@
+public class Program12 {
+    public static void main(String[] args) {
+        String[] fruits = { "Apple", "Orange", "Mango", "jack fruit" };
+        System.out.println(fruits.length);
+    }
+}
