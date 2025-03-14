3c2d2000c8797e18f9a99d8232b7df508f345b43
nishmarnishma <nishmar61@gmail.com>
2025-03-14 14:24:55 +0530

    Arrays

---
+++ Program14.java
@@ -0,0 +1,6 @@
+public class Program14 {
+    public static void main(String[] args) {
+        int[][] num = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println(num[2][0]);
+    }
+}
