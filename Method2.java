338eae3a1df87ea84284c9d3b7b723c701be220a
nishmarnishma <nishmar61@gmail.com>
2025-03-14 13:20:32 +0530

    Checking age using methods

---
+++ Method2.java
@@ -0,0 +1,14 @@
+public class Method2 {
+    static void mymethod() {
+        int Age = 20;
+        if (Age >= 18) {
+            System.out.println("You are eligible for voting");
+        } else {
+            System.out.println("You are not eligible for voting");
+        }
+    }
+
+    public static void main(String[] args) {
+        mymethod();
+    }
+}
