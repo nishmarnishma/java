03b086db174791886ddce934476611b5173d3e23
nishmarnishma <nishmar61@gmail.com>
2025-03-14 14:13:29 +0530

    Multidimentional array

---
+++ Program13.java
@@ -0,0 +1,7 @@
+public class Program13 {
+    public static void main(String[] args) {
+        int[][] numbers = { { 1, 3, 5, 7 }, { 2, 4, 6, 8 } };
+        System.out.println(numbers[1][1]);
+
+    }
+}
