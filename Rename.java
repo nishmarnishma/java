c0e3877575bf2a67aa0af49e911444dade22687b
nishmarnishma <nishmar61@gmail.com>
2025-03-14 14:42:32 +0530

    Rename

---
+++ Rename.java
@@ -0,0 +1,8 @@
+public class Rename {
+    public static void main(String[] args) {
+        String[] str = { "Apple", "Mango", "Pine apple", "Banana" };
+        System.out.println(str[2] = "Orange");
+        
+        
+    }
+}
