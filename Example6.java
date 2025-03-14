d1d88ae59d1e3e91469b3a94ed7d77f46e9d7428
nishmarnishma <nishmar61@gmail.com>
2025-03-14 11:20:20 +0530

    Decrement in for loop

---
+++ Example6.java
@@ -0,0 +1,10 @@
+public class Example6 {
+    public static void main(String[] args) {
+        for (int i = -10; i <= -5; i--) {
+            if (i == -15) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
