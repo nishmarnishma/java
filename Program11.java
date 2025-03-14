1d1c2eb11443e4cdb46523ba5c3ac74f48ace72b
nishmarnishma <nishmar61@gmail.com>
2025-03-14 13:48:19 +0530

    Displaying fruits using Arrays

---
+++ Program11.java
@@ -0,0 +1,10 @@
+public class Program11 {
+    public static void main(String[] args) {
+        String[] fruits = { "Apple", "Orange", "Mango", "Jack fruit" };
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println(fruits[3]);
+
+    }
+}
