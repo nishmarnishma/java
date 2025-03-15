9ae22e9d8404eaefe7a43eb23a6b0b356012e972
nishmarnishma <nishmar61@gmail.com>
2025-03-15 11:12:31 +0530

    Selecting menu

---
+++ Examplequestion3.java
@@ -0,0 +1,21 @@
+public class Examplequestion3 {
+    public static void main(String[] args) {
+        int menu = 3;
+        switch (menu) {
+            case 1:
+                System.out.println("Black coffee");
+                break;
+            case 2:
+                System.out.println("Cold coffee");
+                break;
+            case 3:
+                System.out.println("Hot coffee");
+                break;
+            case 4:
+                System.out.println("Iced coffee");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
