3562981cab5af0ec14c9031fff8e401f550eadb3
nishmarnishma <nishmar61@gmail.com>
2025-03-15 10:52:18 +0530

    Priting price for products

---
+++ Examplequestion2.java
@@ -0,0 +1,10 @@
+public class Examplequestion2 {
+    public static void main(String[] args) {
+        int[] price = { 10, 50, 150, 100, 30 };
+        System.out.println("Price of pencil is " + price[0]);
+        System.out.println("Price of note is " + price[1]);
+        System.out.println("Price of pen is " + price[2]);
+        System.out.println("Price of color pen is" + price[3]);
+        System.out.println("Price of eraser is" + price[4]);
+    }
+}
