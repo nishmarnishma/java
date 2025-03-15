71d91c36a70a68eac27a73f9f66bb5f999551282
nishmarnishma <nishmar61@gmail.com>
2025-03-15 12:07:38 +0530

    Student details

---
+++ User.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class User {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Enter your mark:");
+        int mark = sc.nextInt();
+
+    }
+}
