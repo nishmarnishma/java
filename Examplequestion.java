6348168d4d6edee8036063127d6667fff832de79
nishmarnishma <nishmar61@gmail.com>
2025-03-15 10:40:09 +0530

    Generating invoice number for customer

---
+++ Examplequestion.java
@@ -0,0 +1,8 @@
+public class Examplequestion {
+    public static void main(String[] args) {
+        int[] a = { 101, 102, 103, 104, 105 };
+        for (int i = 0; i <= 4; i++)
+            System.out.println(a[i]);
+    }
+
+}
